de798ac5ab6a090192fdc688102e6995cf8aad73
shengzhc <schen@ancestry.com>
2014-06-25 22:53:57 -0700

    update

--- Pocket VMemo/Helper/PVMRecorder.m
+++ Pocket VMemo/Helper/PVMRecorder.m
@@ -45,6 +45,8 @@
     _isRecording = YES;
     NSDictionary *recordSettings = @{AVSampleRateKey : @(32000.0), AVFormatIDKey : @(kAudioFormatMPEGLayer3), AVNumberOfChannelsKey : @(1), AVEncoderAudioQualityKey : @(AVAudioQualityMax)};
     NSURL *filePath = [NSURL URLWithString:[PVMFileManager getUniqueFilePath]];
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryRecord error:nil];
+    [[AVAudioSession sharedInstance] setActive:YES error:nil];
     self.recorder = [[AVAudioRecorder alloc] initWithURL:filePath settings:recordSettings error:nil];
     self.recorder.delegate = self;
     [self.recorder prepareToRecord];
@@ -58,6 +60,7 @@
         [self.recorder deleteRecording];
         _isRecording = NO;
         self.recorder = nil;
+        [[AVAudioSession sharedInstance] setActive:NO error:nil];
     }
 }
 
@@ -76,6 +79,17 @@
         _isRecording = YES;
     }
 }
+
+- (void)audioRecorderDidFinishRecording:(AVAudioRecorder *)recorder successfully:(BOOL)flag
+{
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+}
+
+- (void)audioRecorderEncodeErrorDidOccur:(AVAudioRecorder *)recorder error:(NSError *)error
+{
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+}
+
 
 
 @end
